ff3e739c65ca264b2df1f896ecddcac2da4237b2
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-04-08 22:21:48 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   int i, start, end;
+
+   start = 1;
+   end = 10;
+   
+   printf("In cac so theo thu tu tang dan:\n");
+   for(i = start; i <= end; i++) 
+      printf("%2d\n", i);
+
+   return 0;
+}
