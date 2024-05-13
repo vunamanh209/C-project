d2cf6436bf9a0a17e71d8eb4106b33f6824801f0
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-05-13 22:58:12 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 
 int main() {
-   int i, start, end;
-
-   start = 1;
-   end = 10;
-   
-   printf("In cac so theo thu tu tang dan:\n");
-   for(i = start; i <= end; i++) 
-      printf("%2d\n", i);
-
-   return 0;
+	int a;
+	int b;
+	int c;
+	
+	printf("enter number a: ");
+	scanf("%d", &a);
+	printf("enter number b: ");
+	scanf("%d", &b);
+	printf("enter number c: ");
+	scanf("%d", &c); 
+	
+	
+	if (a > b && a > c) {
+		printf("a is the biggest number");
+	}
+	else if ( b > a && b > c){
+		printf("b is the biggest number");
+	}
+	
+	else if(c > a && c > b) {
+		printf("c is the biggest number");
+	}
+	else {
+		printf("a and b and c are equal");
+	}
+	return 0;
 }
