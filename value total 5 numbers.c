580ac962a7ef7fb0f2b62341b17105888191b672
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-05-16 00:18:39 +0700

    Add files via upload

--- value total 5 numbers.c
+++ value total 5 numbers.c
@@ -1,5 +1,5 @@
 # include <stdio.h>
-
+/* Calculate the total of up to 5 numbers. If the user enters a negative number, ignore it (do not add the number to the total). */
 int main() {
 
     int number;
@@ -7,14 +7,14 @@ int main() {
 	int i;
 	
     for ( i = 1; i <= 5; ++i){
-        printf("Enter a n%d: ",i);
+        printf("Enter a %d: ",i);
         scanf("%d", &number);
 
         if(number < 0) {
             continue;
         }
 
-        sum += number; // sum = sum + number;
+        sum += number; 
     }
 
     printf("\nSum = %d", sum);
