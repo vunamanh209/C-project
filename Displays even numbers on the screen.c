ea43cfba1e7ad7e98e5b920ac03ce786e3746262
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-06-19 22:47:27 +0700

    Add files via upload

--- Displays even numbers on the screen.c
+++ Displays even numbers on the screen.c
@@ -1,18 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
     int n;
+    
+    // Prompt the user to enter the number of elements
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
+
     int a[1000];
     int i;
+    
+    // Prompt the user to enter the elements
+    printf("Enter the elements: ");
     for (i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
+    // Print even numbers
+    printf("Even numbers are: ");
     for (i = 0; i < n; i++) {
         if (a[i] % 2 == 0) {
             printf("%d ", a[i]);
         }
     }
+    printf("\n"); // Print newline for better output formatting
+
     return 0;
 }
+
