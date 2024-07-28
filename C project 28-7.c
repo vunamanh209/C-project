9353b9f01f93ac8c9cdbb2f8b066d840b6cd3ea4
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-07-29 00:32:04 +0700

    Add files via upload

---
+++ C project 28-7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Program to calculate the sum of numbers (10 numbers max)
+// If the user enters a negative number, the loop terminates
+
+int main() {
+   int i;
+   double number, sum = 0.0;
+
+   for (i = 1; i <= 10; ++i) {
+      printf("Enter n%d: ", i);
+      scanf("%lf", &number);
+      if (number < 0.0) {
+         break;
+      }
+
+      sum += number; 
+   }
+
+   printf("Sum = %.2lf", sum);
+
+   return 0;
+}
