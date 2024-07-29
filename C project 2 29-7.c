6a331363d6c13281325113b9daa4b1fa10a23734
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-07-29 22:16:49 +0700

    Add files via upload

---
+++ C project 2 29-7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i, n;
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+    i = 1;
+    while(i <= n)
+    {
+        if(n % i == 0)
+            printf("%4d", i);
+        i = i + 1;
+    }
+
+    getch();
+    return 0;
+}
---
+++ C project 29-7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i;
+	double number;
+	double sum = 0;
+	
+	for(i; i <= 10; i++){
+		
+		printf("Enter number %d: ", i);
+      	scanf("%lf", &number);
+      	
+		if(number < 0){
+			break;
+		}
+		sum += number;
+	}
+	 printf("Sum = %.2lf", sum);
+	 
+	return 0;
+}
