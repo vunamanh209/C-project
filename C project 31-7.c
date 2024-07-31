e89e93b1c97f784bbeac002ebeb4e72f7978fcf5
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-07-31 20:14:56 +0700

    Add files via upload

--- C project 31-7.c
+++ C project 31-7.c
@@ -4,15 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a = 10; 
-	int b = 15;
+	int a = 10; // Khai bao gia tri cua a bang 10
+	int b = 15; // Khai bao gia tri cua b bang 15
 	int num;
 	
-	int *num1, *num2; // Declare two pointers to int, num1 and num2
-	num1 = &a;    // Assign the address of variable 'a' to pointer 'num1'
-	num2 = &b;    // Assign the address of variable 'b' to pointer 'num2'
+	int *num1, *num2; // Khai bao 2 con tro den kieu bien num1 va num 2
+	num1 = &a; // Gán dia chi cua bien a cho con tro num 1
+	num2 = &b; // Gán dia chi cua bien b cho con tro num 2
 	
-	num = *num1 + *num2; // Dereference num1 and num2 to get the values at the addresses they point to, add them, and assign the result to 'num'
+	num = *num1 + *num2; // Cong 2 gia tri ma dia chi tro den num1 va num 2
 	printf("Total of number = %d", num);
 	return 0;
 }
