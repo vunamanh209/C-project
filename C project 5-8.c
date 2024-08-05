31a460ef2a2a43caf5609498238f783b9de73697
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-08-05 19:53:11 +0700

    Add files via upload

---
+++ C project 5-8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void checkEvenOdd(int num) {
+    if (num % 2 == 0) {
+        printf("%d is even.\n", num);
+    } else {
+        printf("%d is odd.\n", num);
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);  
+    checkEvenOdd(num);  
+    return 0;
+}
+
