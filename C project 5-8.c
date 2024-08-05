37a2d9997826e16d5530dd1fe08cbfc9a7668fd2
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-08-06 00:56:46 +0700

    Add files via upload

--- C project 5-8.c
+++ C project 5-8.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-void checkEvenOdd(int num) {
-    if (num % 2 == 0) {
+void checkEvenOdd(int num) { // Ham khong tra ve gia tri ten checkEvenOdd va nhan tham so la so nguyen num.
+    if (num % 2 == 0) { // If de kiem tra menh de
         printf("%d is even.\n", num);
     } else {
         printf("%d is odd.\n", num);
@@ -11,8 +11,8 @@ void checkEvenOdd(int num) {
 int main() {
     int num;
     printf("Enter a number: ");
-    scanf("%d", &num);  
-    checkEvenOdd(num);  
+    scanf("%d", &num);   // lay so nguoi dung nhap vao
+    checkEvenOdd(num);  // Kiem tra va in ket qua chan le
     return 0;
 }
 
