1a61a6fc9eab9a7167e3231b3cc273f6fcb9fa0b
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-08-06 01:00:33 +0700

    Add files via upload

--- C project 2 5-8.c
+++ C project 2 5-8.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-void addTheNumbers(int a, int b) {
+void addTheNumbers(int a, int b) { // Ham khong tra ve gia tri addTheNumbers va nhan 2 tham so la so nguyen a và b.
     int sum = a + b;
     printf("Sum: %d\n", sum);
 }
 
 int main() {
-    int num1, num2;
+    int num1, num2; // khai bao 2 gia tri num1 va num2
     
-    printf("Enter num1: ");
+    printf("Enter num1: "); // yeu cau nguoi dung nhap so num1
     scanf("%d", &num1);
     
-    printf("Enter num2: ");
+    printf("Enter num2: "); // yeu cau nguoi dung nhap so num2
     scanf("%d", &num2);
     
-    addTheNumbers(num1, num2);
+    addTheNumbers(num1, num2); // goi ham voi 2 tham so va tinh tong
     return 0;
 }
 
