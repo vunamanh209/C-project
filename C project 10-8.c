a4ec6fd744d5adcd1be68edc29df807bc99d4c07
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-08-10 18:32:35 +0700

    Add files via upload

---
+++ C project 10-8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i, n, S;
+	 printf("\nNhap n: ");
+    scanf("%d", &n);
+    S = 0;
+    i = 1;
+    while(i <= n)
+    {
+        S = S + i * i * i;
+        i++;
+    }
+    printf("\nTong la %d", S);
+	return 0;
+}
