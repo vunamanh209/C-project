c87d0127191761cb4e8bb4b88d2f808309e59e92
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2024-08-12 22:33:07 +0700

    Add files via upload

--- C project p2 12-8.c
+++ C project p2 12-8.c
@@ -2,7 +2,7 @@
 
 int countCharacters(char *str) {
     int count = 0;
-    while (*str != '\0') {
+    while (*str != '\0') { //vong lap while tiep tuc chay mien la gia tri ma con tro str tro toi khong phai la ki tu NULL
         count++;
         str++;
     }
@@ -10,8 +10,8 @@ int countCharacters(char *str) {
 }
 
 int main() {
-    char str[] = "Hello, World!";
-    int count = countCharacters(str);
+    char str[] = "Hello, World!"; // chuoi ki tu duoc  khoi tao va luu vao mang str
+    int count = countCharacters(str);//ket qua tra ve tu ham countCharacters dc luu trong bien count
     printf("Number of character in string: %d\n", count);
     return 0;
 }
