608340b6408a9e3695c628d2c7da2bc6f45daaf9
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2025-05-13 22:07:38 +0700

    Add files via upload

---
+++ De bai mon C.c
@@ -0,0 +1,13 @@
+A09.	Nhập 3 số  thực a,b,c  rồi giải và biện luận phương trình bậc 2   ax2 + bx + c = 0 .
+A10.	Viết chương trình  C giải bài toán sau: trăm trâu trăm cỏ, trâu đứng ăn 5, trâu nằm ăn 3, lụ khụ trâu già, ba con ăn 1. Hỏi có bao nhiêu trâu đứng, bao nhiêu trâu nằm và bao nhiêu trâu già?  
+A11.	Nhập số tự nhiên  n  rồi tính  n!  theo công thức: 
+		n! = 1              			nếu n=0
+		    = n*(n-1)*(n-2)*...*2*1	nếu n>0		
+A12. 	Viết chương trình tìm ước số chung lớn nhất  của 2 số nguyên m, n theo thuật toán Euclid.
+A13.	Thời gian làm một việc nào đó là  x  giây. Hãy đọc  x  từ bàn phím và viết chương trình chuyển sang bao nhiêu giờ, phút,giây.
+A14. Viết chương trình nhập số liệu cho ma trận các số thực  A  cấp   mxn   trong đó   m, n là các số tự nhiên. Sau đó tìm ma trận chuyển vị B = (bij)  cấp  nxm, bij = aji i = 1,2,...,n;j =1,2,...,m
+	Cho hiện 2 ma trận trên màn hình để tiện so sánh.
+A15.	Viết chương trình tính tích  2  ma trận các số thực  A  cấp  mxn  và  B   cấp  nxk.
+A16.	Nhập số liệu cho dãy số thực a0 , a1 ,..., an-1  và  một số  x  bất kỳ. Đếm số lần xuất hiện của số  x  trong dãy  trên.
+A17.	Nhập số liệu cho dãy số thực a0 , a1 ,..., an-1 . Tìm  số bé nhất  trong dãy  trên và vị trí của nó trong dãy.
+A18.	Nhập số liệu cho dãy số nguyên a0 , a1 ,..., an-1 Hãy đếm xem trong dãy có bao nhiêu số lẻ và bao nhiêu số chẵn.
