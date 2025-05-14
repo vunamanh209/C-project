335cc6baa81b8732124b0d771189ceb8565fbc5c
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2025-05-15 00:30:54 +0700

    Add files via upload

--- A09.c
+++ A09.c
@@ -1,41 +1,43 @@
 #include<stdio.h>
 #include<math.h>
 
-void giaiptb2(float a, float b, float c){
-	if(a == 0){
-		if(b == 0){
-			if(c == 0){
+void giaiptb2(float a, float b, float c) {
+	if(a == 0) {
+		if(b == 0) {
+			if(c == 0) {
 				printf("Phuong trinh vo so nghiem.");
-			}
-			else{
+			} else {
 				printf("Phuong trinh vo nghiem.");
 			}
+		} else {
+			float x = -c/b;
+			printf("Co 1 nghiem duy nhat: %.2f",x);
+		}
+		return 0;
 	}
-	else{
-		printf("Co 1 nghiem duy nhat: %.2f");
-	}
-	return;
-}
 	float x, x1, x2;
 	float delta = b*b-4*a*c;
-	printf("Delta = %.2f");
-	if(delta < 0){
-		printf("Phuong trinh vo nghiem");
-	}
-	else if(delta == 0){
+	printf("Delta = %.2f.\n",delta);
+	if(delta < 0) {
+		printf("Phuong trinh vo nghiem.\n");
+	} else if(delta == 0) {
 		x = -b / (2 * a);
-	}
-	else{
+		printf("Phuong trinh co 1 nghiem duy nhat.\n");
+		printf("x = %.2f",x);
+	} else {
 		x1 = (-b + sqrt(delta)) / 2 * a;
 		x2 = (-b - sqrt(delta)) / 2 * a;
+		printf("Phuong trinh co 2 nghiem phan biet.\n");
+		printf("x1 = %.2f.\n",x1);
+		printf("x2 = %.2f",x2);
 	}
 }
-	int main(){
-		float a,b,c;
-		printf("Nhap a, b va c :");
-		scanf("%f %f %f", &a, &b, &c);
-	
-		giaiptb2(a,b,c);
+int main() {
+	float a,b,c;
+	printf("Nhap a, b va c :");
+	scanf("%f %f %f", &a, &b, &c);
+
+	giaiptb2(a,b,c);
 	return 0;
 }
-	
+
