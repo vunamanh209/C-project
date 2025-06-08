082c05e569ea1077876b1f54ba7b474706c9ecae
vunamanh209 <166365133+vunamanh209@users.noreply.github.com>
2025-06-09 01:14:03 +0700

    Add files via upload

---
+++ Q1 8.6.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+
+/* Write a C program to make such a pattern like a pyramid with a number which will repeat the number in the same row.
+The pattern is as follows:
+
+   1
+  2 2
+ 3 3 3
+4 4 4 4 */
+
+int main(){
+	int i, j, spc, rows, k;
+	printf("Input number of rows : ");  
+   scanf("%d", &rows);  
+
+   spc = rows + 4 - 1;  
+
+   for(i = 1; i <= rows; i++)  
+   {
+         for(k = spc; k >= 1; k--) 
+         {
+            printf(" ");
+         }
+
+	   for(j = 1; j <= i; j++)  
+	   {
+	       printf("%d ", i);
+	   }
+	
+	printf("\n");  
+    spc--;  
+   }
+	return 0;
+}
